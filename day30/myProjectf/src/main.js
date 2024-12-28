9b6e9b332bbaa733d69b6d7d7ba893ad074ae329
sean23466 <shsiao86@gmail.com>
2024-12-28 23:53:23 +0800

    Update main.js

--- day30/myProjectf/src/main.js
+++ day30/myProjectf/src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import axios from 'axios';//加这句引用
+import axios from 'axios';//加這句引用
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.prototype.$http=axios;//加这句赋值
+Vue.prototype.$http=axios;//加這句赋值
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 /* eslint-disable no-new */
