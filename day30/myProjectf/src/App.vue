a65e109c39870beb0258a112709be95e3a733368
sean23466 <shsiao86@gmail.com>
2024-12-28 23:52:32 +0800

    Update App.vue

--- day30/myProjectf/src/App.vue
+++ day30/myProjectf/src/App.vue
@@ -21,7 +21,7 @@
     const item = {
       date: '2016-05-02',
       name: '王小虎',
-      address: '上海市普陀区金沙江路 1518 弄'
+      address: '上海市普陀區金沙江路 1518 弄'
     };
     return {
       tableData: Array(20).fill(item)
